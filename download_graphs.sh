63d082592e42eb41120dd5aeef5aa66c1411b5f1
David Valarezo <davidelian200@gmail.com>
2026-04-21 16:33:47 -0700

    (feat): download script

---
+++ download_graphs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Run this once on Perlmutter to download all graph data
+# Run with bash download_graphs.sh
+
+mkdir -p $SCRATCH/graphs
+cd $SCRATCH/graphs
+
+echo "Downloading roadNet-CA..."
+wget -nc https://snap.stanford.edu/data/roadNet-CA.txt.gz
+gunzip -f roadNet-CA.txt.gz
+
+echo "Downloading com-LiveJournal..."
+wget -nc https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+gunzip -f soc-LiveJournal1.txt.gz
+
+echo "Done. Graphs are in $SCRATCH/graphs/"
+ls -lh $SCRATCH/graphs/
